342d5264a2ab5a6afe47e30da14b95c763bc4f29
kxx <132kxx@gmail.com>
2022-06-28 16:27:46 +0900

    connect tabview

---
+++ Twitter Clone/Core/Messages/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  Twitter Clone
+//
+//  Created by kxx: on 2022/06/28.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text("Messages")
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
